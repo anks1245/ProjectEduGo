71453e5c0b1e92a03e0c05336d6cc5ed143ae243
anks1245 <anks1245@gmail.com>
2021-02-16 00:07:00 +0530

    SplashScreen.kt updated

--- app/src/main/java/com/android/example/projecthackathon/SplashScreen.kt
+++ app/src/main/java/com/android/example/projecthackathon/SplashScreen.kt
@@ -1,8 +1,11 @@
 package com.android.example.projecthackathon
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.view.WindowManager
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -13,8 +16,13 @@ class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT < 16) {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        }
         setContentView(R.layout.activity_splash_screen)
-
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         //setting animation
         val imageView: ImageView = findViewById(R.id.splashImage)
         imageView.animation = AnimationUtils.loadAnimation(this, R.anim.splash_anim)
