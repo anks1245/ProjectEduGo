149104fdd2052df53786255b7a6d5564371cb317
Aman Raj <amanrj2914@gmail.com>
2021-02-16 17:24:52 +0530

    changed the design of log in and updated the app icon

---
+++ app/src/main/java/com/android/example/projecthackathon/login_signup/LoginFragment.kt
@@ -0,0 +1,20 @@
+package com.android.example.projecthackathon.login_signup
+
+import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.android.example.projecthackathon.R
+
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+}
