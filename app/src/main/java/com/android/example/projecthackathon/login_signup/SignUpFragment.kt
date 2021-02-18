6e9c7b266a15a4da162b57db79057af75c776c9d
Aman Raj <amanrj2914@gmail.com>
2021-02-18 20:10:52 +0530

    added SignUp layout

---
+++ app/src/main/java/com/android/example/projecthackathon/login_signup/SignUpFragment.kt
@@ -0,0 +1,19 @@
+package com.android.example.projecthackathon.login_signup
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.android.example.projecthackathon.R
+
+class SignUpFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+}
