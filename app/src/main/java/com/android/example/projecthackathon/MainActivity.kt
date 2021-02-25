ef827b01252a935e771827f860b36e6eb8e3eef1
Aman Raj <amanrj2914@gmail.com>
2021-02-25 23:12:45 +0530

    slider image

--- app/src/main/java/com/android/example/projecthackathon/MainActivity.kt
+++ app/src/main/java/com/android/example/projecthackathon/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.android.example.projecthackathon
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.android.example.projecthackathon.home.HomeFragment
 import com.android.example.projecthackathon.job.JobFragment
 import com.android.example.projecthackathon.message.MessageFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.smarteist.autoimageslider.SliderLayout
+import com.smarteist.autoimageslider.SliderView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,10 +27,10 @@ class MainActivity : AppCompatActivity() {
         setCurrentFragment(homeFragment)
 
         bottomNavigationView.setOnNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.home->setCurrentFragment(homeFragment)
-                R.id.job->setCurrentFragment(jobFragment)
-                R.id.message->setCurrentFragment(messageFragment)
+            when (it.itemId) {
+                R.id.home -> setCurrentFragment(homeFragment)
+                R.id.job -> setCurrentFragment(jobFragment)
+                R.id.message -> setCurrentFragment(messageFragment)
 
             }
             true
--- app/src/main/java/com/android/example/projecthackathon/home/HomeFragment.kt
+++ app/src/main/java/com/android/example/projecthackathon/home/HomeFragment.kt
@@ -5,16 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.android.example.projecthackathon.R
+import com.smarteist.autoimageslider.SliderLayout
+import com.smarteist.autoimageslider.SliderView
 
 class HomeFragment : Fragment() {
 
+    private lateinit var sliderLayout: SliderLayout
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        sliderLayout = view.findViewById(R.id.imageSlider)
+        sliderLayout.setIndicatorAnimation(SliderLayout.Animations.WORM)
+        sliderLayout.scrollTimeInSec = 1
+        setSliderViews()
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return view
     }
 
+    private fun setSliderViews() {
+
+        val resources = listOf(R.drawable.ic_home, R.drawable.ic_job, R.drawable.ic_message)
+        for (i in resources) {
+            val sliderView = SliderView(context)
+            //sliderView.setImageDrawable(i)
+            sliderView.setImageScaleType(ImageView.ScaleType.CENTER_CROP)
+            sliderLayout.addSliderView(sliderView)
+        }
+    }
 }
