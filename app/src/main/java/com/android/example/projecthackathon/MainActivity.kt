e37a8de47ef679a8a0f7457f3662a09b0cd69ae6
Aman Raj <amanrj2914@gmail.com>
2021-02-26 23:42:26 +0530

    done

--- app/src/main/java/com/android/example/projecthackathon/MainActivity.kt
+++ app/src/main/java/com/android/example/projecthackathon/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.android.example.projecthackathon
 
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.android.example.projecthackathon.home.HomeFragment
 import com.android.example.projecthackathon.job.JobFragment
 import com.android.example.projecthackathon.message.MessageFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.smarteist.autoimageslider.SliderLayout
-import com.smarteist.autoimageslider.SliderView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +26,17 @@ class MainActivity : AppCompatActivity() {
         val jobFragment = JobFragment()
         val messageFragment = MessageFragment()
 
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
 
+//        drawerLayout = findViewById(R.id.drawerLayout)
+//        val navHostFragment =
+//            supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+//        val navController: NavController = navHostFragment.navController
+//
+//        NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
+//        val navView: NavigationView = findViewById(R.id.navView)
+//        NavigationUI.setupWithNavController(navView, navController)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
         bottomNavigationView.selectedItemId = R.id.home
         setCurrentFragment(homeFragment)
 
@@ -40,7 +54,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun setCurrentFragment(fragment: Fragment) =
         supportFragmentManager.beginTransaction().apply {
-            replace(R.id.flFragment, fragment)
+            replace(R.id.myNavHostFragment, fragment)
             commit()
         }
+
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = this.findNavController(R.id.myNavHostFragment)
+//        return NavigationUI.navigateUp(navController, drawerLayout)
+//    }
 }
