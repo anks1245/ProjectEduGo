30fbbb952974905d9c7623f5f49a63643cf99511
anks1245 <anks1245@gmail.com>
2021-02-27 10:07:19 +0530

    updated

---
+++ app/src/main/java/com/android/example/projecthackathon/MentorAppointment.kt
@@ -0,0 +1,11 @@
+package com.android.example.projecthackathon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MentorAppointment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mentor_appointment)
+    }
+}
